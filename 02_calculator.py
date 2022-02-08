7161e58fa8da6900d14004e2dc6be7ae4a99681f
gopinathmech13 <49205518+gopinathmech13@users.noreply.github.com>
2022-02-08 11:07:30 +0530

    Update 02_calculator.py

--- 02_calculator.py
+++ 02_calculator.py
@@ -3,34 +3,38 @@ num1 =float(input("enter a number : "))
 while True: 
     
     para=input("press (+,-,*,/) or press q to quit : ")
-    if para=="q":
+    if para!="q" and para!="+" and para!="-" and para!="*" and para!="/":
+        print("Invalid input, please try again !!")
+        continue
+
+    elif para=="q":
         result=num1
-        print("final result=",result)
-        break
+        print("final result = ",result)
+        break 
     num2=float(input("enter another number : "))
     if para =="+" :
         #num2=0
         result=num1+num2
-        print("result=",result)
+        print("result so far = ",result)
 
         
     elif para =="-" :
         #num2=0
         result=num1-num2
-        print("result=",result)
+        print("result so far =",result)
 
     elif para =="*" :
         #num2=1
         result=num1*num2
-        print("result=",result)
+        print("result so far = ",result)
 
     elif para =="/" :
         #num2=1
         result=num1/num2
-        print("result=",result)
+        print("result so far = ",result)
 
-    else:
-        print("printing mistake")
-        continue
+    elif para=="q":
+        print("final result = ",result)
+        break
     num1=result
-#print("result=",result)
+
